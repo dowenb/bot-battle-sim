811889d3d578abe3f0d31d4f3267de137acf3c41
Ben Dowen <dirtydevil@gmail.com>
2024-01-19 17:33:29 +0000

    Get bots

--- index.js
+++ index.js
@@ -3,25 +3,42 @@ const app = express()
 app.use(express.json());
 const port = 3000
 
-const blueBots = {}
-const redBots = {}
+const blueBots = []
+const redBots = []
 
+function newBot(port) {
+  return {
+    botNumber: Math.floor(Math.random() * 9999),
+    botPort:   botPort = port
+  }
+}
 app.post('/register/blue', (req, res) => {
-    let randomBlueBotNumber = Math.floor(Math.random() * 9999)
-    let botPort = req.body.port
-    blueBots.randomBlueBotNumber = botPort
-    res.send(randomBlueBotNumber.toString())
-    console.log(`Blue Bot ${randomBlueBotNumber} registered, with port ${botPort}`)
-  })
+  const blueBot = newBot(req.body.port)
+  blueBots.push(blueBot)
+  res.send(blueBot)
+  console.log(`Blue Bot ${blueBot.botNumber} registered, with port ${blueBot.port}`)
+})
 
-  app.post('/register/red', (req, res) => {
-    let randomRedBotNumber = Math.floor(Math.random() * 9999)
-    let botPort = req.body.port
-    redBots.randomRedBotNumber = botPort
-    res.send(randomRedBotNumber.toString())
-    console.log(`Red Bot ${randomRedBotNumber} registered, with port ${botPort}`)
-  })
+app.post('/register/red', (req, res) => {
+  const redBot = newBot(req.body.port)
+  redBots.push(redBot)
+  res.send(redBot)
+  console.log(`Red Bot ${redBot.botNumber} registered, with port ${redBot.botNumber}`)
+})
+
+app.get('/bots/blue', (req, res) => {
+  res.send(blueBots)
+  console.log(`List of Blue Bots: \n${JSON.stringify(blueBots)}`)
+}
+)
+
+app.get('/bots/red', (req, res) => {
+  res.send(redBots)
+  console.log(`List of Red Bots: \n${JSON.stringify(redBots)}`)
+}
+)
 
 app.listen(port, () => {
+
   console.log(`Example app listening on port ${port}`)
 })
